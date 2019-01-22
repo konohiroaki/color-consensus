3230d0495fdcd3631a1e26002c140fa474a70638
Hiroaki Kono <hiroakikono1990@gmail.com>
2019-01-22 20:20:56 +0900

    make it work with react + webpack + babel

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const title = "My Minimal React Webpack Babel Setup";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<div>{title}あああああ</div>, document.getElementById("app"));

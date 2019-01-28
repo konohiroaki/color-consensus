ff922b3c646892fb8d2e89bd74a7caba75317d54
Hiroaki Kono <hiroakikono1990@gmail.com>
2019-01-28 22:36:51 +0900

    implement basic api

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,11 +1,19 @@
 module.exports = {
     entry: "./src/index.js",
     module: {
-        rules: [{
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: ["babel-loader"]
-        }]
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            }, {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    {loader: "css-loader", options: {modules: true}}
+                ],
+            }
+        ]
     },
     resolve: {
         extensions: ["*", ".js", ".jsx"]

720b99f38f75c2bdd546dd849a81e402ee4a5619
Hiroaki Kono <hiroakikono1990@gmail.com>
2019-01-30 05:39:44 +0900

    use command line argument for specifying env

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"flag"
 	"github.com/konohiroaki/color-consensus/backend/config"
 	"github.com/konohiroaki/color-consensus/backend/server"
 )
 
 func main() {
-	config.Init("development")
+	env := flag.String("env","production", "specify environment")
+	flag.Parse()
+	config.Init(*env)
 
 	server.Init()
 }

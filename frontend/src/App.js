bb226d71c070b9330beb5b61e509706c10edbfe5
Hiroaki Kono <hiroakikono1990@gmail.com>
2019-01-30 22:19:05 +0900

    use bootstrap and make dark page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,28 @@ class App extends Component {
 
     render() {
         return (
-            <div>
+            <div className="bg-dark text-light" style={{display: "flex", flexDirection: "column", height: "100%"}}>
+                <Header style={{flex: "0 0 80px"}}/>
+                <div style={{flex: "1 1 auto", display: "flex", flexDirection: "row"}}>
+                    <div>
+                        asdfsdfsadfasdfsdfsadfasdfsdfsadfasdfsdfsadf
+                        asdfsdfsadfasdfsdfsadfasdfsdfsadf
+                    </div>
+                    <div style={{borderLeft: "#999 solid 1px", flex: "0 0 20em"}}>
+                        asdfaskdjfa;slkdjf;alskdjf;alksdjf;alksdjf;alskdjf;kj
+                    </div>
+                </div>
             </div>
+        );
+    }
+}
+
+class Header extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-dark bg-dark border-bottom border-primary" style={this.props.style}>
+                <a className="navbar-brand" href="/">Color Consensus</a>
+            </nav>
         );
     }
 }
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
                 test: /\.css$/,
                 use: [
                     "style-loader",
-                    {loader: "css-loader", options: {modules: true}}
+                    "css-loader"
                 ],
             }
         ]

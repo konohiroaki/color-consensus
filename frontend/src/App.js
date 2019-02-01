2b17d583734031ca6f482825bca1d505b6b71dc0
Hiroaki Kono <hiroakikono1990@gmail.com>
2019-02-01 19:35:03 +0900

    now sidebar shows actual color list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import React, {Component} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import axios from "axios";
 
 class App extends Component {
 
@@ -33,8 +35,13 @@ class MainContent extends Component {
         };
     }
 
+    //TODO: should draw when triggered.
+    static draw(lang, name, code) {
+        console.log(lang + ":" + name + ":" + code);
+    }
+
     componentDidMount() {
-        // calculate similar colors
+        //TODO: get color from sidebar? then draw
     }
 
     render() {
@@ -53,49 +60,96 @@ class SideBar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            // api call result.
+            colorList: []
         };
+        this.updateColorList = this.updateColorList.bind(this);
     }
 
     componentDidMount() {
-        // call api. axios.
+        // TODO: remove domain when releasing.
+        axios.get("http://localhost:5000/api/v1/colors/keys").then(this.updateColorList);
+    }
+
+    updateColorList({data}) {
+        this.setState({colorList: data});
     }
 
     render() {
-        // TODO: create color card from api result.
-        // TODO: when click, it should draw the main content for it. (maybe <button> is better than <a>)
-        // TODO: add search box at the top for better UX.
+        // FIXME: make the search box work.
+        // TODO: implement add modal.
+        let colorList = [];
+        let langSet = new Set();
+        for (let v of this.state.colorList) {
+            colorList.push(<ColorCard lang={v.lang} name={v.name} code={v.base_code} key={v.lang + ":" + v.name}/>);
+            langSet.add(v.lang);
+        }
+        let langList = [];
+        for (let v of langSet) {
+            langList.push(<div className="dropdown-item" key={v}>{v}</div>);
+        }
+
         return (
-            <div className={this.props.className} style={{overflowY: "auto", width: "20em"}}>
-                <ColorCard/>
-                <ColorCard/>
-                <ColorCard/>
-                <ColorCard/>
-                <ColorCard/>
-                <ColorCard/>
-                <ColorCard/>
-                <ColorCard/>
-                <ColorCard/>
-                <ColorCard/>
-                <ColorCard/>
-                <ColorCard/>
-                <ColorCard/>
-                <ColorCard/>
-                <ColorCard/>
-                <ColorCard/>
-                <ColorCard/>
-                <ColorCard/>
+            <div className={this.props.className}>
+                <div className="input-group">
+                    <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown">Language</button>
+                    <div className="dropdown-menu">
+                        {langList}
+                    </div>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div style={{overflowY: "auto", height: "100%"}}>
+                    {colorList}
+                    <AddColorCard/>
+                    <ColorCard/>
+                    <ColorCard/>
+                    <ColorCard/>
+                    <ColorCard/>
+                    <ColorCard/>
+                    <ColorCard/>
+                    <ColorCard/>
+                    <ColorCard/>
+                    <ColorCard/>
+                    <ColorCard/>
+                </div>
             </div>
         );
     }
 }
 
 class ColorCard extends Component {
+    constructor(props) {
+        super(props);
+        this.render.bind(this);
+    }
+
+    handleClick(lang, name, code) {
+        MainContent.draw(lang, name, code);
+    }
+
     render() {
         return (
-            <a className="card bg-dark border border-secondary m-2" href="/">
-                <div className="card-body text-light text-center">
-                    ja : 赤 (10 Votes)
+            <a className="card btn bg-dark border border-secondary m-2" onClick={this.handleClick.bind(this, this.props.lang, this.props.name, this.props.code)}>
+                <div className="card-body">
+                    {this.props.lang} : {this.props.name}
+                </div>
+            </a>
+        );
+    }
+}
+
+import {faPlus} from "@fortawesome/free-solid-svg-icons";
+
+class AddColorCard extends Component {
+
+    static handleClick(e) {
+
+    }
+
+    render() {
+        return (
+            <a className="card btn bg-dark border border-secondary m-2" onClick={ColorCard.handleClick}>
+                <div className="card-body">
+                    <FontAwesomeIcon icon={faPlus}/>
                 </div>
             </a>
         );

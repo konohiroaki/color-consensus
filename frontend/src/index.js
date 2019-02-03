120aa5a8b9c90bcc2936e9d347074c690bf4752d
Hiroaki Kono <hiroakikono1990@gmail.com>
2019-02-03 11:57:16 +0900

    draw border when selected

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,13 @@ import ReactDom from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap";
 import "./test.css";
+
+import "core-js/fn/object/assign";
+import "core-js/fn/array/from";
+import "core-js/fn/array/is-array";
+import "core-js/fn/map";
+import "core-js/fn/set";
+
 import App from "./App";
 
 ReactDom.render(<App/>, document.getElementById("app"));

0b10090c094eeaae6ad8d5e5cfda114e4db07bf0
Hiroaki Kono <hiroakikono1990@gmail.com>
2019-02-10 20:19:03 +0900

    silly refactoring

--- frontend/src/components/body/main/CandidateCell.js
+++ frontend/src/components/body/main/CandidateCell.js
@@ -3,7 +3,12 @@ import {createSelectable} from "react-selectable-fast";
 
 export class CandidateCell extends Component {
 
-    shouldComponentUpdate(nextProps, nextState) {
+    constructor(props) {
+        super(props);
+        this.cellSize = "13px";
+    }
+
+    shouldComponentUpdate(nextProps) {
         // when color is changed, need to update
         if (this.props.color !== nextProps.color) {
             return true;
@@ -13,20 +18,22 @@ export class CandidateCell extends Component {
             return false;
         }
         // when selected, selecting state is different, need to update
-        return nextProps.selected !== this.props.selected || nextProps.selecting !== this.props.selecting;
+        return this.props.selected !== nextProps.selected || this.props.selecting !== nextProps.selecting;
     }
 
     render() {
         console.log("rendering candidate cell");
-        const cellSize = "13px";
-        let color = this.props.selected || this.props.selecting ? "#fff" : "transparent";
+        const color = this.props.selected || this.props.selecting ? "#fff" : "transparent";
         return (
             <div style={{
                 display: "inline-block", padding: "1px", margin: "0 -1px -1px 0",
-                borderWidth: "1px", borderStyle: "solid", borderColor: color,
+                borderWidth: "1px", borderStyle: "solid",
+                borderTopColor: color, borderRightColor: color,
+                borderBottomColor: color, borderLeftColor: color,
                 userSelect: "none", userDrag: "none"
             }}>
-                <div ref={this.props.selectableRef} style={{width: cellSize, height: cellSize, backgroundColor: this.props.color}}/>
+                <div ref={this.props.selectableRef}
+                     style={{width: this.cellSize, height: this.cellSize, backgroundColor: this.props.color}}/>
             </div>
         );
     }

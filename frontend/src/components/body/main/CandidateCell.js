7635d79c5de042fa4138b80fdf9a2f2c0c6e00c6
Hiroaki Kono <hiroakikono1990@gmail.com>
2019-02-11 18:22:56 +0900

    draw border for group of selected candidates instead of each selected candidates

--- frontend/src/components/body/main/CandidateCell.js
+++ frontend/src/components/body/main/CandidateCell.js
@@ -5,12 +5,19 @@ export class CandidateCell extends Component {
 
     constructor(props) {
         super(props);
-        this.cellSize = "13px";
+        this.cellSize = "15px";
     }
 
     shouldComponentUpdate(nextProps) {
         // when color is changed, need to update
         if (this.props.color !== nextProps.color) {
+            return true;
+        }
+        // when border state changed, need to update
+        if (this.props.border.top !== nextProps.border.top
+            || this.props.border.right !== nextProps.border.right
+            || this.props.border.bottom !== nextProps.border.bottom
+            || this.props.border.left !== nextProps.border.left) {
             return true;
         }
         // when selecting -> selected, no need to update
@@ -23,19 +30,28 @@ export class CandidateCell extends Component {
 
     render() {
         console.log("rendering candidate cell");
-        const color = this.props.selected || this.props.selecting ? "#fff" : "transparent";
         return (
             <div style={{
                 display: "inline-block", padding: "1px", margin: "0 -1px -1px 0",
                 borderWidth: "1px", borderStyle: "solid",
-                borderTopColor: color, borderRightColor: color,
-                borderBottomColor: color, borderLeftColor: color,
+                borderTopColor: CandidateCell.getBorderColor(this.props.border.top),
+                borderRightColor: CandidateCell.getBorderColor(this.props.border.right),
+                borderBottomColor: CandidateCell.getBorderColor(this.props.border.bottom),
+                borderLeftColor: CandidateCell.getBorderColor(this.props.border.left),
                 userSelect: "none", userDrag: "none"
             }}>
                 <div ref={this.props.selectableRef}
                      style={{width: this.cellSize, height: this.cellSize, backgroundColor: this.props.color}}/>
             </div>
         );
+    }
+
+    static getBorderColor(hasBorder) {
+        return hasBorder ? "#fff" : "transparent";
+    }
+
+    componentDidUpdate() {
+        this.props.setCellState(this.props.selected || this.props.selecting);
     }
 }
 

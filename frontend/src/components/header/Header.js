7f02dee6dff011257085fbfbd8aeddce44f5b644
Hiroaki Kono <hiroakikono1990@gmail.com>
2019-02-16 23:18:04 +0900

    implement skeleton of user authentication

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,17 +1,42 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 class Header extends Component {
 
-    shouldComponentUpdate() {
-        return false;
+    constructor(props) {
+        super(props);
+        this.state = {
+            userId: null,
+        };
     }
 
     render() {
+        const userId = this.state.userId;
+        const button = userId === null || userId === ""
+                       ? null
+            // TODO: click button to copy userId into clipboard
+                       : <button className="btn btn-outline-secondary">ID: {userId}</button>;
+        console.log("rendering header");
         return (
             <nav className="navbar navbar-dark bg-dark border-bottom border-secondary" style={this.props.style}>
                 <a className="navbar-brand" href="/">Color Consensus</a>
+                {button}
             </nav>
         );
+    }
+
+    componentDidMount() {
+        axios.get("http://localhost:5000/api/v1/users/presence")
+            .then(({data}) => {
+                // TODO: show user id on header
+                console.log("user present", data);
+                this.setState({userId: data.userID});
+            })
+            .catch(() => {
+                // TODO: show modal to login or sign up.
+                console.log("user not present");
+                this.setState({userId: ""});
+            });
     }
 }
 

5ac63aa9e040c9396e669a20ed3a493e7ebf49ce
Hiroaki Kono <hiroakikono1990@gmail.com>
2019-02-19 23:18:22 +0900

    move signup/login modal from header to common dir

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,14 +1,31 @@
 import React, {Component} from "react";
 import Header from "./header/Header";
 import Body from "./body/Body";
+import LoginModal from "./common/LoginModal";
 
 // TODO: do test for react app (jest?)
 class App extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {};
+        /* userId: undefined -> haven't checked user login status.
+         *         null      -> user isn't logged in.
+         *         "string"  -> user is logged in.
+         */
+
+        this.setUserId = this.setUserId.bind(this);
+    }
+
+    setUserId(userId) {
+        this.setState({userId: userId});
+    }
+
     render() {
         return (
             <div className="bg-dark text-light" style={{display: "flex", flexDirection: "column", height: "100%"}}>
-                <Header style={{flex: "0 0 80px"}}/>
+                <LoginModal setUserId={this.setUserId} userId={this.state.userId}/>
+                <Header style={{flex: "0 0 80px"}} userId={this.state.userId}/>
                 <Body style={{flex: "1 1 auto"}}/>
             </div>
         );
--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,142 +1,28 @@
 import React, {Component} from "react";
-import axios from "axios";
-import $ from "jquery";
 
 class Header extends Component {
 
-    constructor(props) {
-        super(props);
-        // TODO: lift up the login state because it's not header's state.
-        this.state = {};
-
-        this.handleSignUpClick = this.handleSignUpClick.bind(this);
-        this.handleLoginClick = this.handleLoginClick.bind(this);
-    }
-
     render() {
-        const userId = this.state.userId;
+        const userId = this.props.userId;
         const button = userId === undefined || userId === null
                        ? this.signUpLoginButton()
             // TODO: click button to copy userId into clipboard
                        : <button className="btn btn-outline-secondary">ID: {userId}</button>;
-        const modal = userId === null
-                      ? this.modal()
-                      : null;
         console.log("rendering header");
         return (
             <nav className="navbar navbar-dark bg-dark border-bottom border-secondary" style={this.props.style}>
                 <a className="navbar-brand" href="/">Color Consensus</a>
                 {button}
-                {modal}
             </nav>
         );
     }
 
     signUpLoginButton() {
+        // TODO: get modal from props to depart from tight couple?
         return (<button className="btn btn-outline-light"
                         data-toggle="modal" data-target="#signup-login-modal">
             Sign Up / Login
         </button>);
-    }
-
-    modal() {
-        return (
-            <div className="modal fade" tabIndex="-1" id="signup-login-modal">
-                <div className="modal-dialog modal-dialog-centered">
-                    <div className="modal-content bg-dark">
-                        <div className="modal-body">
-                            <ul className="nav nav-tabs" role="tablist"
-                                style={{
-                                    // deny .modal-body's 1em padding for right and left.
-                                    marginLeft: "-1em", marginRight: "-1em",
-                                    // deny .nav style and use <ul> style
-                                    marginBottom: "1rem", paddingLeft: "40px"
-                                }}>
-                                <li className="nav-item">
-                                    {/* TODO: proper text color for nav link*/}
-                                    <a className="nav-link active" href="#signup-tab" data-toggle="tab" role="tab">Sign Up</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#login-tab" data-toggle="tab" role="tab">Login</a>
-                                </li>
-                            </ul>
-                            <div className="tab-content" id="myTabContent">
-                                <div className="tab-pane fade show active" id="signup-tab" role="tabpanel">
-                                    {/* TODO: get list from server and use select box */}
-                                    Nationality:
-                                    <input type="text" className="form-control" id="add-user-nationality" placeholder="Japan"
-                                           onChange={e => this.setState({nationality: e.target.value})}/>
-                                    {/* TODO: get list from server and use select box */}
-                                    Gender:
-                                    <input type="text" className="form-control" id="add-user-gender" placeholder="Male"
-                                           onChange={e => this.setState({gender: e.target.value})}/>
-                                    {/* TODO: get list from server and use select box */}
-                                    Birth:
-                                    <input type="text" className="form-control" id="add-user-birth" placeholder="yyyy"
-                                           onChange={e => this.setState({birth: e.target.value})}/>
-                                    <div className="modal-footer" style={{paddingBottom: "0"}}>
-                                        <button type="button" className="btn btn-primary" data-dismiss="modal"
-                                                onClick={this.handleSignUpClick}>
-                                            Submit
-                                        </button>
-                                    </div>
-                                </div>
-                                <div className="tab-pane fade" id="login-tab" role="tabpanel">
-                                    ID:
-                                    <input type="text" className="form-control" id="login-user-id" placeholder="asdf"
-                                           onChange={e => this.setState({userIdInput: e.target.value})}/>
-                                    <div className="modal-footer" style={{paddingBottom: "0"}}>
-                                        <button type="button" className="btn btn-primary" data-dismiss="modal"
-                                                onClick={this.handleLoginClick}>
-                                            Submit
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
-    componentDidMount() {
-        axios.get("http://localhost:5000/api/v1/users/presence")
-            .then(({data}) => {
-                console.log("user present", data);
-                this.setState({userId: data.userID});
-            })
-            .catch(() => {
-                console.log("user not present");
-                this.setState({userId: null});
-            });
-    }
-
-    // TODO: even after closing the modal, give a way or force user to login/signup before doing anything else.
-    componentDidUpdate() {
-        if (this.state.userId === null) {
-            $("#signup-login-modal").modal();
-        }
-    }
-
-    handleSignUpClick() {
-        axios.post("http://localhost:5000/api/v1/users", {
-            nationality: this.state.nationality,
-            gender: this.state.gender,
-            birth: Number(this.state.birth)
-        }).then(({data}) => {
-            this.setState({userId: data.id});
-        });
-    }
-
-    handleLoginClick() {
-        axios.post("http://localhost:5000/api/v1/users/presence", {id: this.state.userIdInput})
-            .then(({data}) => {
-                this.setState({userId: data.userID});
-            })
-            .catch(() => {
-                // TODO: error handling
-            });
     }
 }
 

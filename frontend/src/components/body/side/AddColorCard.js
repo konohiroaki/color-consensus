6caf566b3b1b505449c3ea78d3d6ce2fa7fc057e
Hiroaki Kono <hiroakikono1990@gmail.com>
2019-02-23 13:47:01 +0900

    remove jquery from add color card component

--- frontend/src/components/body/side/AddColorCard.js
+++ frontend/src/components/body/side/AddColorCard.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import $ from "jquery";
 import axios from "axios";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPlus} from "@fortawesome/free-solid-svg-icons";
@@ -13,11 +12,8 @@ class AddColorCard extends Component {
             name: "",
             code: ""
         };
-        this.handleClick = this.handleClick.bind(this);
-    }
 
-    shouldComponentUpdate() {
-        return false;
+        this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick() {
@@ -25,10 +21,6 @@ class AddColorCard extends Component {
             .then(() => {
                 this.props.updateColorList();
             });
-        // TODO: should get rid of jquery. need to make it controlled component?
-        $("#add-color-lang").val("");
-        $("#add-color-name").val("");
-        $("#add-color-code").val("");
         this.setState({lang: "", name: "", code: ""});
     }
 
@@ -41,31 +33,45 @@ class AddColorCard extends Component {
                         <FontAwesomeIcon icon={faPlus}/>
                     </div>
                 </a>
-                <div className="modal fade" tabIndex="-1" id="color-add-modal">
-                    <div className="modal-dialog modal-dialog-centered">
-                        <div className="modal-content bg-dark">
-                            <div className="modal-header">
-                                <span className="modal-title">Add Color</span>
-                            </div>
-                            <div className="modal-body">
-                                {/* TODO: should be drop down */}
-                                Language:
-                                <input type="text" className="form-control" id="add-color-lang" placeholder="en"
-                                       onChange={e => this.setState({lang: e.target.value})}/>
-                                Color Name:
-                                <input type="text" className="form-control" id="add-color-name" placeholder="red"
-                                       onChange={e => this.setState({name: e.target.value})}/>
-                                Base Color Code:
-                                <input type="text" className="form-control" id="add-color-code" placeholder="#ff0000"
-                                       onChange={e => this.setState({code: e.target.value})}/>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                                {/* TODO: dismiss the modal only when submit is success */}
-                                <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={this.handleClick}>
-                                    Add Color
-                                </button>
-                            </div>
+                <AddColorModal lang={this.state.lang}
+                               langSetter={e => this.setState({lang: e.target.value})}
+                               name={this.state.name}
+                               nameSetter={e => this.setState({name: e.target.value})}
+                               code={this.state.code}
+                               codeSetter={e => this.setState({code: e.target.value})}
+                               handleClick={this.handleClick}/>
+            </div>
+        );
+    }
+}
+
+class AddColorModal extends Component {
+    render() {
+        return (
+            <div className="modal fade" tabIndex="-1" id="color-add-modal">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content bg-dark">
+                        <div className="modal-header">
+                            <span className="modal-title">Add Color</span>
+                        </div>
+                        <div className="modal-body">
+                            {/* TODO: should be drop down */}
+                            Language:
+                            <input type="text" className="form-control" id="add-color-lang" placeholder="en"
+                                   value={this.props.lang} onChange={this.props.langSetter}/>
+                            Color Name:
+                            <input type="text" className="form-control" id="add-color-name" placeholder="red"
+                                   value={this.props.name} onChange={this.props.nameSetter}/>
+                            Base Color Code:
+                            <input type="text" className="form-control" placeholder="#ff0000"
+                                   value={this.props.code} onChange={this.props.codeSetter}/>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                            {/* TODO: dismiss the modal only when submit is success */}
+                            <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={this.props.handleClick}>
+                                Add Color
+                            </button>
                         </div>
                     </div>
                 </div>

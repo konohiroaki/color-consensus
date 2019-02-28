f8709e12d68bf3759e3c4218bb5989137b33600a
Hiroaki Kono <hiroakikono1990@gmail.com>
2019-03-01 00:59:10 +0900

    add gray in sample data

--- backend/repository/data.go
+++ backend/repository/data.go
@@ -11,6 +11,7 @@ var Consensus = []*models.ColorConsensus{
 	{Language: "en", Color: "red", Code: "#ff0000", Vote: 10, Colors: map[string]int{"ff0000": 10, "#ff007f": 3}},
 	{Language: "en", Color: "green", Code: "#008000", Vote: 10, Colors: map[string]int{"00ff00": 10, "#00ff33": 3}},
 	{Language: "ja", Color: "赤", Code: "#bf1e33",Vote: 15, Colors: map[string]int{"ff0000": 15, "#ff007f": 5}},
+	{Language: "en", Color: "gray", Code: "#808080",Vote: 0, Colors: map[string]int{}},
 }
 
 var Votes = []*models.ColorVote{

31fd5bc5cf7810d0f5538fd1370afa99c05f11fe
Hiroaki Kono <hiroakikono1990@gmail.com>
2019-03-06 00:43:04 +0900

    add selectbox for filtering the result of statistics

--- frontend/src/components/body/main/StatisticsPage.js
+++ frontend/src/components/body/main/StatisticsPage.js
@@ -9,12 +9,36 @@ class StatisticsPage extends Component {
 
         return (
             <div>
-                <div className="row">
-                    <div className="mr-auto ml-5">
-                        <p>Language: {this.props.target.lang}</p>
-                        <p>Color Name: {this.props.target.name}</p>
+                <div className="card bg-dark border border-secondary">
+                    <div className="card-body">
+                        <div className="row ml-0 mr-0">
+                            <div className="col-3 card bg-dark border border-secondary p-2 text-center">
+                                <div className="row">
+                                    <span className="col-4 border-right border-secondary p-3">{this.props.target.lang}</span>
+                                    <span className="col-8 p-3">{this.props.target.name}</span>
+                                </div>
+                            </div>
+                            {/* TODO: complete select box impl */}
+                            <div className="col-9 input-group">
+                                <select className="custom-select">
+                                    <option selected>Nationality</option>
+                                    <option value="3">Others</option>
+                                </select>
+                                <select className="custom-select">
+                                    <option selected>Age Group</option>
+                                    <option value="3">Others</option>
+                                </select>
+                                <select className="custom-select">
+                                    <option selected>Gender</option>
+                                    <option value="1">Male</option>
+                                    <option value="2">Female</option>
+                                    <option value="3">Others</option>
+                                </select>
+                            </div>
+                        </div>
                     </div>
-
+                </div>
+                <div className="row">
                     <div className="ml-auto">
                         <button className="btn btn-secondary m-3" onClick={() => this.props.history.push("/")}>
                             Back to voting

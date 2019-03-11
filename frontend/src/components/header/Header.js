16828b7a9d1dbbea5f88615f1f7315c9e8274dfe
Hiroaki Kono <hiroakikono1990@gmail.com>
2019-03-12 07:30:23 +0900

    clean up header code

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -7,52 +7,51 @@ class Header extends Component {
 
     constructor(props) {
         super(props);
-
-        this.signUpLoginButton = this.signUpLoginButton.bind(this);
     }
 
     render() {
         console.log("rendering header", this.props.userId);
-        const userId = this.props.userId;
-        const button = userId === undefined || userId === null
-                       ? this.signUpLoginButton()
-                       : Header.userIdButton(userId);
-        return (
-            <nav className="navbar navbar-dark bg-dark border-bottom border-secondary" style={this.props.style}>
-                <div>
-                    <a className="navbar-brand" href="/">Color Consensus</a>
-                    <GithubLink/>
-                </div>
-                {button}
-            </nav>
-        );
-    }
 
-    signUpLoginButton() {
-        return (
-            <button className="btn btn-outline-light" onClick={() => this.props.loginModalRef.openLoginModal()}>
-                Sign Up / Login
-            </button>
-        );
-    }
-
-    static userIdButton(userId) {
-        // TODO: show a dialog(?) to notify a user that id is copied to their clipboard.
-        return (
-            <CopyToClipboard text={userId}>
-                <button className="btn btn-outline-secondary">
-                    ID: {userId}
-                </button>
-            </CopyToClipboard>
-        );
+        return <nav className="navbar navbar-dark bg-dark border-bottom border-secondary" style={this.props.style}>
+            <HeaderLeftPart/>
+            <HeaderRightPart userId={this.props.userId} loginModalRef={this.props.loginModalRef}/>
+        </nav>;
     }
 }
+
+const HeaderLeftPart = () => (
+    <div>
+        <a className="navbar-brand" href="/">Color Consensus</a>
+        <GithubLink/>
+    </div>
+);
 
 const GithubLink = () => (
     // FIXME: placement of icon is not good. place it a bit lower.
     <a href="https://github.com/konohiroaki/color-consensus" className="text-light">
         <FontAwesomeIcon icon={faGithub} size="2x"/>
     </a>
+);
+
+const HeaderRightPart = (props) => {
+    return props.userId === undefined || props.userId === null
+           ? <SignUpLoginModalButton loginModalRef={props.loginModalRef}/>
+           : <UserIdCopyButton userId={this.userId}/>;
+};
+
+const SignUpLoginModalButton = (props) => (
+    <button className="btn btn-outline-light" onClick={() => props.loginModalRef.openLoginModal()}>
+        Sign Up / Login
+    </button>
+);
+
+// TODO: show a dialog(?) to notify a user that id is copied to their clipboard.
+const UserIdCopyButton = (props) => (
+    <CopyToClipboard text={props.userId}>
+        <button className="btn btn-outline-secondary">
+            ID: {props.userId}
+        </button>
+    </CopyToClipboard>
 );
 
 export default Header;

9884377899055137f07b3ec8a2c402a2ffdfdf10
Hiroaki Kono <hiroakikono1990@gmail.com>
2019-03-12 07:09:41 +0900

    clean up code in statistics page (except the board component)

--- frontend/src/components/body/main/statistics/ColorCell.js
+++ frontend/src/components/body/main/statistics/ColorCell.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 
-class ResultCell extends Component {
+class ColorCell extends Component {
 
     constructor(props) {
         super(props);
@@ -28,20 +28,18 @@ class ResultCell extends Component {
             <div style={{
                 display: "inline-block", padding: "1px",
                 borderWidth: "1px", borderStyle: "solid",
-                borderTopColor: ResultCell.getBorderColor(this.props.border.top),
-                borderRightColor: ResultCell.getBorderColor(this.props.border.right),
-                borderBottomColor: ResultCell.getBorderColor(this.props.border.bottom),
-                borderLeftColor: ResultCell.getBorderColor(this.props.border.left),
+                borderTopColor: getBorderColor(this.props.border.top),
+                borderRightColor: getBorderColor(this.props.border.right),
+                borderBottomColor: getBorderColor(this.props.border.bottom),
+                borderLeftColor: getBorderColor(this.props.border.left),
                 userSelect: "none", userDrag: "none"
             }}>
                 <div style={{width: this.cellSize, height: this.cellSize, backgroundColor: this.props.color}}/>
             </div>
         );
     }
-
-    static getBorderColor(hasBorder) {
-        return hasBorder ? "#fff" : "transparent";
-    }
 }
 
-export default ResultCell;
+const getBorderColor = (hasBorder) => hasBorder ? "#fff" : "transparent";
+
+export default ColorCell;

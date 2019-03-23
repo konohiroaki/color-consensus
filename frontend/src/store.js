ef10e28a3f3c71e92341c0faa93e4db0eafb6e0d
Hiroaki Kono <hiroakikono1990@gmail.com>
2019-03-23 22:19:48 +0900

    introduce redux and store color list (but not using it yet)

---
+++ frontend/src/store.js
@@ -0,0 +1,18 @@
+import {createStore, combineReducers, applyMiddleware} from "redux";
+import {reducer as freezer} from "./ducks/freezer";
+import {reducer as orders} from "./ducks/orders";
+import {reducer as employees} from "./ducks/employees";
+import {reducer as colors} from "./ducks/colors";
+import thunk from "redux-thunk";
+import {composeWithDevTools} from "redux-devtools-extension";
+
+const rootReducer = combineReducers({
+    // freezer,
+    // orders,
+    // employees,
+    colors,
+});
+
+export default createStore(rootReducer, composeWithDevTools(
+    applyMiddleware(thunk)
+));

ef0eb8259099fcf46779e9b706cbfebf8a379188
Hiroaki Kono <hiroakikono1990@gmail.com>
2019-04-02 00:01:51 +0900

    change /votes/findBy... endpoint to /votes, and generalize for get operation

--- backend/controllers/vote.go
+++ backend/controllers/vote.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/konohiroaki/color-consensus/backend/domains/user"
 	"github.com/konohiroaki/color-consensus/backend/domains/vote"
+	"strings"
 )
 
 type VoteController struct{}
@@ -26,11 +27,20 @@ func (VoteController) Vote(c *gin.Context) {
 	c.Status(200)
 }
 
-func (VoteController) FindVotes(c *gin.Context) {
-	lang := c.Param("lang")
-	color := c.Param("name")
-	list := vote.FindByLangAndName(lang, color)
-	c.JSON(200, list)
+func (VoteController) GetVotes(c *gin.Context) {
+	type request struct {
+		Lang   string `form:"lang"`
+		Name   string `form:"name"`
+		Fields string `form:"fields"`
+	}
+	var req request
+	if (c.ShouldBind(&req)) == nil {
+		fields := strings.Split(req.Fields, ",")
+		votes := vote.GetVotes(req.Lang, req.Name, fields)
+		c.JSON(200, votes)
+		return
+	}
+	c.AbortWithStatus(400)
 }
 
 func (VoteController) DeleteVotesForUser(c *gin.Context) {

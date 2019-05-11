20fb8d8894035b84c43bec9a766e6228c35cd582
Hiroaki Kono <hiroakikono1990@gmail.com>
2019-05-12 00:33:28 +0900

    unify style in login modal and remove redundant file

--- frontend/src/components/common/Utility.js
+++
@@ -1,2 +0,0 @@
-export const isSameColor = (c1, c2) => c1 !== undefined && c2 !== undefined
-                                       && c1.lang === c2.lang && c1.name === c2.name && c1.code === c2.code;
